4e23375987f468b040428a9a1bd67ff4a676e754
Bowei Zhang <zbw0517@gmail.com>
2025-08-30 20:42:31 -0700

    basic daemon

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "tbdd")]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Start,
+    Stop,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Start => {
+            println!("Starting tmux-botdomo daemon...");
+            // TODO: Implement
+        }
+        Command::Stop => {
+            println!("Stopping tmux-botdomo daemon...");
+            // TODO: Implement
+        }
+    }
 }
