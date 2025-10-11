de7c2bc67eb1e452e462626381d6c4f034c9540b
Bowei Zhang <zbw0517@gmail.com>
2025-10-11 01:18:05 -0700

    Revert "go agane?"
    This reverts commit 7de3d70d9c29452db9ae2181ecdbb5beb8b91089.

--- src/unix.rs
+++ src/unix.rs
@@ -1,13 +1,11 @@
 pub fn get_pid_file_path() -> String {
-    std::env::var("TMUX_BOTDOMO_PID_PATH").unwrap_or_else(|_| {
-        // TODO: XDG_RUNTIME_DIR?
-        let session_id = get_tmux_session_id();
-        format!(
-            "/tmp/tmux-botdomo-{}-{}.pid",
-            std::env::var("USER").unwrap_or_else(|_| "unknown".to_string()),
-            session_id,
-        )
-    })
+    // TODO: XDG_RUNTIME_DIR?
+    let session_id = get_tmux_session_id();
+    format!(
+        "/tmp/tmux-botdomo-{}-{}.pid",
+        std::env::var("USER").unwrap_or_else(|_| "unknown".to_string()),
+        session_id,
+    )
 }
 
 pub fn get_socket_path() -> String {
